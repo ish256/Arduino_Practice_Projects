7dfa73753bd801d96531907592b37fc7c0f73d98
Ismael Xique <ixique@umich.edu>
2017-07-30 20:09:22 -0400

    update everyone has a thumb code
    update of everyone has a thumb code to include lcd view

--- everyone_has_a_thumb/everyone_has_a_thumb.ino
+++ everyone_has_a_thumb/everyone_has_a_thumb.ino
@@ -1,3 +1,5 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(9, 8, 5, 4, 3, 2);
 const int sensorPin = A1;
 const int moistureAO = 0;
 int AO = 0;
@@ -12,7 +14,11 @@ void setup () {
   pinMode(moistureAO, INPUT);
   pinMode(buzzPin, OUTPUT);
   pinMode(LED,OUTPUT);
-
+  lcd.begin(16, 2);
+  lcd.setCursor(0,0);
+  lcd.print("Temp:");
+  lcd.setCursor(0,1);
+  lcd.print("Humidity:");
 }
 
 void loop() {
@@ -21,14 +27,16 @@ void loop() {
   float temperature = (voltage - .5)*100;
   Serial.println("temperature: ");
   Serial.println(temperature);
-
+  lcd.setCursor(7,0);
+  lcd.print(temperature);
   tmp = analogRead( moistureAO );
 
   if (tmp != AO) {
   AO = tmp;
   Serial.print("\t A = ");
   Serial.println(AO);
-
+  lcd.setCursor(11,1);
+  lcd.print(AO);
   }
   
   delay( 1000 );
